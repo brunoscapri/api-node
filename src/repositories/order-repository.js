a15ce3e37258cbdd7364467a937968a0811ad4f4
brunoscapri <bruno.capriglioni@pucpr.edu.br>
2020-03-25 13:49:35 -0300

    att get no repositorio de pedidos

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async () => {
-    var res = await Order.find({});
+    var res = await Order.find({}, 'number status items')
+        .populate('customer', 'name email')
+        .populate('items.product', 'title slug price')
+
     return res;
 }
 
